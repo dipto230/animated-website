6934b52fcf1ca458fe479ac520548048036da9e3
dipto230 <diptoroy230305@gmail.com>
2024-11-26 18:48:33 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Hero from './components/Hero'
+import About from './components/About'
+
+const App = () => {
+  return (
+    <main className="relative min-h-screen w-screen overflow-x-hidden">
+   <Hero>
+   
+   </Hero>
+  <About></About>
+    </main>
+  )
+}
+
+export default App
